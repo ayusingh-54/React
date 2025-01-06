1a7f061cf8d56d5dc0eed0294db64d1422e14095
Ayush Singh <2023071025@mmmut.ac.in>
2025-01-06 19:42:32 +0530

    first commit

--- todolist/src/component/Addtodo2.jsx
+++ todolist/src/component/Addtodo2.jsx
@@ -2,7 +2,7 @@ function Addtodo2() {
     return (<div>
         <div class="container text-center" className="my-2">
             <div class="row">
-                <div class="col-5"> Buy Milk</div>
+                <div class="col-5"> pay Milk</div>
                 <div class="col-3">04/10/2024</div>
                 <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
             </div>
